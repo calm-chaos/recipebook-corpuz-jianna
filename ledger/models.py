3dad7db3dded7454d3bd718140758492351da68f
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-03-15 00:24:35 +0800

    added profile model and author field in recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -1,35 +1,56 @@
 from django.db import models
 from django.urls import reverse
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
 class Ingredient(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return "{}".format(self.name)
 
     def get_absolute_url(self):
-        return reverse('recipes:ingredients', args=[self.pk])
-    
+        return reverse("recipes:ingredients", args=[self.pk])
+
+
 class Recipe(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
+
+    author = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="recipe"
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return "{}".format(self.name)
 
     def get_absolute_url(self):
-        return reverse('ledger:recipe', args=[self.pk])
+        return reverse("ledger:recipe", args=[self.pk])
 
-class RecipeIngredient(models.Model): #linking recipe to ingredient
-    quantity = models.CharField(max_length = 50)
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
 
     ingredient = models.ForeignKey(
-        'Ingredient',
-        on_delete=models.CASCADE,
-        related_name='recipe'
+        "Ingredient", on_delete=models.CASCADE, related_name="recipe"
     )
     recipe = models.ForeignKey(
-        'Recipe',
-        on_delete = models.CASCADE,
-        related_name = 'ingredients'
+        "Recipe", on_delete=models.CASCADE, related_name="ingredients"
     )
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(255, "This field must contain at least 255 characters")
+        ]
+    )
+
+    def __str__(self):
+        return "{}".format(self.name)
